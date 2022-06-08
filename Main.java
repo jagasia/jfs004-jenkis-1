5c118634ed08940a773ba5b11a208595491f3b2b
Jagadeeswaran <rjagadeeswaran@yahoo.com>
2022-06-08 09:27:10 +0530

    jun08_2

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main
 	public static void main(String args[])
 	{
 		System.out.println("Hello jenkins runs my java program from git.");
+		System.out.println("JFS004 batch created this....");
 	}
 }
